27e611ce598648bc91ec5523f251368bd5c14d2d
GreenShadowMaker <shadowgit@greenghost.net>
2018-07-01 15:53:35 -0700

    fix lower and set rgb LED count

--- keyboards/iris/keymaps/greenshadowmaker/config.h
+++ keyboards/iris/keymaps/greenshadowmaker/config.h
@@ -33,7 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
+// Connected 1 LED only
+#define RGBLED_NUM 1
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
